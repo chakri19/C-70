6339cf5d954dd876b698fda172932bc5c18d3f4b
riceeater19 <68394708+chakri19@users.noreply.github.com>
2021-02-16 16:28:52 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator, createSwitchNavigator} from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import WriteStoryScreen from './screens/WriteStoryScreen';
+import ReadStoryScreen from './screens/ReadStoryScreen';
+
+export default class App extends Component {
+    render(){
+        return(
+            <View style = {{flex:1}}>
+                <AppContainer/>
+            </View>
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+var tabNavigator = createBottomTabNavigator({
+    WriteStoryScreen: WriteStoryScreen,
+    ReadStoryScreen: ReadStoryScreen
+})
+
+var switchNavigator = createSwitchNavigator({
+    TabNavigator: {screen: tabNavigator}
+})
+
+const AppContainer = createAppContainer(switchNavigator);
